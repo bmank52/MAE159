720a78c5b8f8f941919c509ecf3fbb8ea305ce74
bmank52 <bmank5252@gmail.com>
2026-01-14 13:27:54 -0800

    add fuel weight ratio estimate function

---
+++ fuel_weight_ratio_estimate.py
@@ -0,0 +1,9 @@
+from ambiance import Atmosphere as atm, Atmosphere
+import numpy as np
+
+def fuel_weight_ratio_estimate(range, M_cruise, altitude):   #range in nmi, alt in ft
+    coeffes = [5.66594337e-13, -1.23328283e-08, 1.25236419e-04, 6.14364245e-03]
+    v_kts = M_cruise * Atmosphere(altitude * 0.3048).speed_of_sound * 1.94384 # Converts M to kts
+    all_out_range = range + 200 + .75 * v_kts # Mission range + 200 nmi + 45mins cruise speed
+    print(all_out_range)
+    return np.polyval(coeffes, all_out_range)
