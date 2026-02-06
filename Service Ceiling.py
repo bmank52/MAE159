4d8dba01b003a590ae8c65f5b7f934a82564cc95
bmank52 <bmank5252@gmail.com>
2026-02-06 13:26:08 -0800

    Created Service Ceiling Module

---
+++ Service Ceiling.py
@@ -0,0 +1,48 @@
+import numpy as np
+from drag_constant_Cdo import drag_constant_Cdo
+from ambiance import Atmosphere
+from Propulsion import propulsion
+
+
+def service_ceiling(W_S, h_ceiling, h_cruise=35000, AR = 10, M_cruise = 0.85, ROC=100):
+    Cd, L_D, Cdo, e = drag_constant_Cdo(1, 1, AR, False, False, 0, 0.85)  #inputs CL, CL max, M not needed
+    rho_ceiling = Atmosphere(h_ceiling * 0.3048).density[0] * 0.00194032
+    K = 1 / (e * AR * np.pi)
+    CL_best_L_D = np.sqrt(Cdo / K)
+    V_best_L_D = np.sqrt(2 * W_S * 0.965 / rho_ceiling / CL_best_L_D)
+    thrust_max, lapse = propulsion(h_ceiling, M_cruise)
+
+    T_W_guess = 0.4
+    T_W_climb = T_W_guess * lapse
+    eta = T_W_climb * np.sqrt(1 / (Cdo * K))
+
+    V_ratio = np.sqrt( (eta + np.sqrt(eta ** 2 + 12)) / 6) #ratio Vy to V_best_L_D
+    Vy = V_best_L_D * V_ratio
+    CLy = CL_best_L_D / V_ratio ** 2
+    a_ceiling = (Atmosphere(h_ceiling * 0.3048).speed_of_sound[0] * 3.28084)
+    My = Vy / a_ceiling
+
+    V_cruise = M_cruise * Atmosphere(h_cruise * 0.3048).speed_of_sound[0] * 3.28084
+    rho_cruise = Atmosphere(h_cruise * 0.3048).density[0] * 0.00194032
+    CL_cruise = 2 * W_S / (rho_cruise * V_cruise ** 2)
+
+    Mdiv_cruise = M_cruise + 0.004
+    delta_Mdiv = np.polyval([0.84225806, -1.77681462, 1.03674548, -0.17259473], CL_cruise)
+    Mdiv_CL_55 = Mdiv_cruise - delta_Mdiv  # Solves for Mdiv at CL = .55
+    delta_Mdiv_CL_y = np.polyval([ 0.84225806, -1.77681462,  1.03674548, -0.17259473], CLy)
+    Mddy = Mdiv_CL_55 - delta_Mdiv_CL_y
+
+    while Mddy < My + 0.004:
+        My = Mddy - 0.004
+        Vy = My * a_ceiling
+        CLy = 2 * W_S * 0.965 / (rho_ceiling * Vy **2)
+        delta_Mdiv_CL_y = np.polyval([0.84225806, -1.77681462, 1.03674548, -0.17259473], CLy)
+        Mddy = Mdiv_CL_55 - delta_Mdiv_CL_y
+
+    L_D_climb = drag_constant_Cdo(CLy, 3, AR, False, False, My, Mddy)[1] #CL max not needed because in cruise config
+    T_W_climb = 1 / L_D_climb + (ROC/60) / Vy
+    lapse_climb = propulsion(h_ceiling, My)[1]
+    T_W = T_W_climb * 0.965/lapse_climb
+
+    return T_W
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 import pandas as pd
+from approach_speed import iterate_W_S_LND
 
 ### Design Specs ###
 n_pax = 250
@@ -13,4 +14,21 @@ M_cruise = 0.85
 cruise_alt = 35000 #ft
 max_landing_fuel_ratio = 0.45
 b_limit = 214 #ft
+temp_delta = 22.6 #hot day added temp in K
+
+### Input Ranges
+W_S = 140
+sweep = 35
+AR = 10
+
+### Constraint Diagram
+plt.figure()
+W_S_Approach = iterate_W_S_LND(W_S, M_cruise, sweep, AR, landing_approach_speed, temp_delta, 0)
+
+plt.axvline(x = W_S_Approach, color = 'red', label = 'Approach')
+
+plt.legend()
+plt.xlabel('W_S')
+plt.ylabel('T_W')
+plt.show()
 
