4ef4e409699a986bbde37ca96ccd66a6b7e45444
bmank52 <bmank5252@gmail.com>
2026-02-06 22:28:33 -0800

    Made updates to repair LNDFL module

--- Landing_Field_Length.py
+++ Landing_Field_Length.py
@@ -4,12 +4,12 @@ from fuel_weight_ratio_estimate import fuel_weight_ratio_estimate
 from ambiance import Atmosphere
 import numpy as np
 
-def calc_landing_FL_W_S(W_S,  M_cruise, sweep, AR, density, alt, Cdo, LNDFL):
+def calc_landing_FL_W_S(W_S,  M_cruise, sweep, AR, density, alt, Cdo, LNDFL, max_fuel_LND_ratio, range):
     CL_max_LND = Mdd_constraint(W_S, M_cruise, alt, sweep, AR)[1]
 
     ##Descent
     CL = CL_max_LND / (1.3**2)
-    L_D_descent = CL / drag_constant_Cdo(CL, AR, True, 'LND',0, M_cruise, Cdo)
+    L_D_descent = CL / drag_constant_Cdo(CL, CL_max_LND,AR,  True, 'LND',0, M_cruise, Cdo)[0]
     h_obs = 50 #obstance height
     c1 = LNDFL * 0.6 - L_D_descent * h_obs
     c2 = 0.3675 * L_D_descent / (density * 32.2 * CL_max_LND)
@@ -19,24 +19,23 @@ def calc_landing_FL_W_S(W_S,  M_cruise, sweep, AR, density, alt, Cdo, LNDFL):
     Cd_lnd_roll = drag_constant_Cdo(CL_lnd_gnd_roll, AR, True, 'LND', 0, M_cruise, Cdo)[0]
     mu = 0.22 #Friction coefficent
     c3 = 1/ (density * 32.2 * (mu * CL_lnd_gnd_roll - Cd_lnd_roll))
-    c4 = -mu * CL_max_LND / (mu * CL_max_LND + 1.3225 * (mu*CL_lnd_gnd_roll - Cd_lnd_roll))
+    c4 = -mu * CL_max_LND / (-mu * CL_max_LND + 1.3225 * (mu*CL_lnd_gnd_roll - Cd_lnd_roll))
 
     W_S_LND = c1 / (c2 + c3 * np.log(c4))
-    W_S_TO = W_S_LND / (1-(1-0.45) *  fuel_weight_ratio_estimate(7000, M_cruise, alt))
+    W_S_TO = W_S_LND / (1-(1- max_fuel_LND_ratio) *  fuel_weight_ratio_estimate(range, M_cruise, alt))
     return W_S_TO
 
-def iterate_W_S_LND(W_S,  M_cruise, sweep, AR, alt=0, Cdo=0.015, LNDFL=9000, temp_delta=22.6):
+def iterate_W_S_LND(W_S,  M_cruise, sweep, AR, alt=0, Cdo=0.015, LNDFL=9000, max_fuel_LND_ratio=0.45, range = 7000, temp_delta=22.6):
     densityHot = Atmosphere(0).pressure[0] / 287 / (Atmosphere(0).temperature[0] + temp_delta) * 0.00194032  # convert to lbf/ft^3
-    W_S_new = calc_landing_FL_W_S(W_S, M_cruise, sweep, AR, densityHot, alt, Cdo, LNDFL)
+    W_S_new = calc_landing_FL_W_S(W_S, M_cruise, sweep, AR, densityHot, alt, Cdo, LNDFL, max_fuel_LND_ratio, range)
 
-    while(np.abs(W_S_new - W_S) > 0.0001):
+    while np.abs(W_S_new - W_S) > 0.001:
         W_S = W_S_new
-        W_S_new = calc_landing_FL_W_S(W_S, M_cruise, sweep, AR, densityHot, alt, Cdo, LNDFL)
+        W_S_new = calc_landing_FL_W_S(W_S, M_cruise, sweep, AR, densityHot, alt, Cdo, LNDFL, max_fuel_LND_ratio, range)
 
     return W_S
 
-
-
+print(iterate_W_S_LND(130, 0.82, 35, 10, 35000, 0.015, 6000, 0.4, 3000))
 
 
 
